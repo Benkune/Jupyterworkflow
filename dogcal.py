c52fc5b0773b5f6eb823d1184a641a6e7e691772
BENARD OSANG <benardosang@benards-mbp.home>
2018-07-29 14:20:40 +0100

    it's mine

---
+++ dogcal.py
@@ -0,0 +1,7 @@
+dog_name = input("what is your dog's name? ")
+dog_age = input("what is your dog's age? ")
+human_age = int(dog_age)*7
+print(human_age)
+
+print('your dog', dog_name, 'is', human_age, 'years old in in human years')
+                 
